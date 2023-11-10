1ac23da9b7a7213ff884f6fe1e5f650603287054
Kim Weast <weastkim3@gmail.com>
2023-11-10 13:15:26 -0600

    Merge changes from remote

---
+++ App.jsx
@@ -0,0 +1,46 @@
+/* eslint-disable no-unused-vars */
+import Mybutton from './components/MyButton';
+import './App.css';
+
+function App() {
+  return (  
+    <div className="App">
+      <header className="App-logo" alt="logo">
+        <h1>Learning React</h1>
+        <p>React is so cool!</p>
+        <p>Here is another paragraph</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ul>
+          <li>One</li>
+          <li>Two</li>
+          <li>Three</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+
+
+          
+
+
+
+        
+       
+  
+
+      
+       
+
+
+  
+
+export default App
---
+++ info.jsx
@@ -0,0 +1,15 @@
+function Info(){
+    return(
+        <> 
+        <h2>Finding Nemo</h2>
+        <p>A clownfish father trys to locate his missing son.</p>
+        <h3>Favorite Characters:</h3>
+        <ul>
+            <li>Dory</li>
+            <li>Crush</li>
+            <li>Bruce</li>
+       </ul>
+       </>
+    )
+}
+export default Info;
